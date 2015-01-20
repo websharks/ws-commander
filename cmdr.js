6b6a97240dd7dac93b2b600b459b79d705b1933b
Bruce <bruce@myinbox.ws>
2015-01-20 04:51:27 -0500

    Initial commit

---
+++ cmdr.js
@@ -0,0 +1,42 @@
+(function()
+{
+	// Allows usage of `window` variable instead of `GLOBAL` as needed.
+	GLOBAL.window = GLOBAL;
+
+	var fs = require('fs'),
+		notify = require('osx-notifier'),
+		readline = require('readline');
+
+	var opts = {};
+
+	if(fs.existsSync(__dirname + '/cmdr.json'))
+		opts = require(__dirname + '/cmdr.json');
+
+	if(!opts.user_name) console.error('No basic config found. Please run `npm setup` from the `ws-commander` installation directory before trying to use this tool.');
+
+	/* === Done Basic Setup === */
+
+	/* === Global Utilities Setup === */
+	GLOBAL.ws = {};
+
+	/**
+	 * Creates a desktop notification (for OSX only)
+	 *
+	 * @param title string
+	 * @param msg string
+	 *
+	 * Usage: `ws.notify('Test Subject / Title', 'Message to display in Notification');`
+	 */
+	ws.notify = function(title, msg)
+	{
+		notify({
+			       type      : "info",
+			       "title"   : "WS Commander",
+			       "subtitle": title,
+			       "message" : msg,
+			       "group"   : "ws-commander"
+		       });
+	};
+
+	/* === Etc. ==== */
+})();
---
+++ utils/setup.js
@@ -0,0 +1,73 @@
+(function()
+{
+	var opts = {}; // Options object
+
+	var fs = require('fs'),
+		notify = require('osx-notifier'),
+		readline = require('readline');
+
+	var rl = readline.createInterface({input: process.stdin, output: process.stdout});
+
+	/* === Initial Build Information === */
+
+	console.log("\n" + 'This script will take you through account setup for various portions of the WebSharks Commander.');
+
+	/**
+	 * Prompt for initial "Name" argument
+	 */
+	rl.question('To begin with, what should I call you?: ', function(name)
+	{
+		opts.user_name = name;
+		console.log('Thank-you, ' + name + '. I\'ll call you that from now on.');
+
+		gitHubPrompt();
+	});
+
+	/**
+	 * Prompt for GitHub integration details
+	 */
+	var gitHubPrompt = function()
+	{
+		rl.question('Would you like to set up the GitHub integration? [y/N]: ', function(doGitHub)
+		{
+			doGitHub = doGitHub.toLowerCase().charAt(0) === 'y';
+
+			if(doGitHub)
+			{
+				console.log('Okay, let\'s set up GitHub.');
+
+				rl.question('GitHub Personal Access API Key: ', function(key)
+				{
+					opts.github_api_key = key;
+				});
+
+				finish();
+			}
+			else
+			{
+				console.log('Okay, moving on.');
+				finish();
+			}
+		});
+	};
+
+	/**
+	 * Wraps everything up
+	 */
+	var finish = function()
+	{
+		rl.close(); // Stops execution after function ends.
+
+		var json = JSON.stringify(opts);
+		fs.writeFileSync(__dirname + '/../cmdr.json', json);
+
+		console.log("\n" + 'That\'s it! Thanks for setting up the WebSharks Commander! Your settings have been saved.');
+		notify({
+			       type      : "info",
+			       "title"   : "WS Commander",
+			       "subtitle": "Setup Complete",
+			       "message" : "You have successfully set up the WebSharks Commander.",
+			       "group"   : "ws-commander"
+		       });
+	}
+})();
