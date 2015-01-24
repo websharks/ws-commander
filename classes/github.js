568c2ef4f0cf25724ee3bb83caadfd1f26c99f74
Bruce <bruce@myinbox.ws>
2015-01-24 16:46:48 -0500

    Finishing ws github next-issue-url

--- classes/github.js
+++ classes/github.js
@@ -22,8 +22,45 @@
 
 			if(typeof repoInfo === undefined) return;
 
-			console.log(repoInfo);
+			module.exports.retrieveNextIssueID(repoInfo.owner, repoInfo.repo);
 		}
 		else return console.error('Not a valid use of `ws github`.');
+	};
+
+	module.exports.retrieveNextIssueID = function(owner, repo)
+	{
+		var https = require('https');
+
+		var opts = {
+			hostname: 'api.github.com',
+			port    : 443,
+			path    : '/repos/' + owner.toLowerCase() + '/' + repo.toLowerCase() + '/issues',
+			method  : 'GET',
+			headers : {
+				'User-Agent': 'WebSharks Commander'
+			}
+		};
+
+		var req = https.request(opts, function(res)
+		{
+			var body = '';
+
+			res.setEncoding('utf8');
+			res.on('data', function(chunk)
+			{
+				body += chunk.toString();
+			});
+
+			res.on('end', function()
+			{
+				var issues = JSON.parse(body);
+				if(!issues.length || issues.hasOwnProperty('documentation_url')) return console.error('Error accessing GitHub API');
+
+				var nextIssueNum = parseInt(issues[0].number) + 1;
+				console.log('https://github.com/' + owner + '/' + repo + '/issues/' + nextIssueNum);
+			});
+		});
+
+		req.end();
 	};
 })();
